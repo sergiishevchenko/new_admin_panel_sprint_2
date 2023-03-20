1eec2508bb443bf6625a3577facc897706d14f7e
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-03-20 19:52:57 +0100

    added debug_toolbar

--- docker_compose/simple_project/app/example/urls.py
+++ docker_compose/simple_project/app/example/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+import debug_toolbar
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('movies.api.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
