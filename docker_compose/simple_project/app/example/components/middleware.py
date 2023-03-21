74be47d041cb15db4a9f4ab7c01d333f1955f31b
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-03-21 11:06:23 +0100

    added debug_toolbar.middleware

--- docker_compose/simple_project/app/example/components/middleware.py
+++ docker_compose/simple_project/app/example/components/middleware.py
@@ -7,4 +7,5 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
